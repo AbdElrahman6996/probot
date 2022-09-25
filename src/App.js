a6adf4acb0308f8fb8b69284c50d2139f4477a2d
Abdelrahman Amir <abdoamir6996@gmail.com>
2022-09-26 01:01:57 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import './App.css'
 import Hero from "./components/Hero";
+import WhyProbot from "./components/WhyProbot";
 
 function App() {
   return (
     <>
       <Header/>
       <Hero/>
+      <WhyProbot/>
     </>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import DiscordLogo from '../icons/discordicon.svg'
+import './Hero.css'
 const Hero = () => {
   return (
     <main>
@@ -7,7 +8,8 @@ const Hero = () => {
             <h1>Make A Professional Discord Server!</h1>
         </div>
         <div className="main-btn-container">
-
+            <a href="/" className='add-to-discord-btn'><img src={DiscordLogo} alt="" />Add To Discord</a>
+            <a href="/" className='hero-login-btn'>Login</a>
         </div>
     </main>
   )
