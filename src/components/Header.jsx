54c57d98a0cdffb5b3dae94cbb44d373773b9559
Abdelrahman Amir <abdoamir6996@gmail.com>
2022-09-26 00:27:48 +0200

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import LogoSvg from '../icons/logo2.svg'
+import CommandIcon from '../icons/commandIcon.svg'
+import SupportIcon from '../icons/questionmark.svg'
+import UsFlag from '../icons/us.png'
 import './Header.css'
+
 const Header = () => {
   return (
     <header>
         <div>
             <a href="/"><img src={LogoSvg} alt="" /></a>
         </div>
+        <nav className='app-navbar'>
+            <a href="/"><img src={CommandIcon} alt="commands-icon" />Commands</a>
+            <a href="/"><img src={SupportIcon} alt="support-icon" />Supportr</a>
+            <a href="/"><img src={UsFlag} alt="language-icon" /></a>
+        </nav>
     </header>
   )
 }
